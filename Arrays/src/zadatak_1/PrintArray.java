23ef82dc62adc10602afddee8c5a2a76273c4c76
Luka <artukovfax@gmail.com>
2025-04-22 15:33:10 +0200

    prvi zadatak

---
+++ Arrays/src/zadatak_1/PrintArray.java
@@ -0,0 +1,12 @@
+package zadatak_1;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int [] niz={1,2,3,4,5,6,7,8,9,10};
+        System.out.println("clanovi niza su");
+        for (int i:  niz){
+            System.out.println(i);
+        }
+    }
+
+}
