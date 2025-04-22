778df9f7e2efc6dfd9e73704dac4c7833026bf3c
Luka <artukovfax@gmail.com>
2025-04-22 15:39:32 +0200

    ispravka

--- Arrays/src/zadatak_1/PrintArray.java
+++ Arrays/src/zadatak_1/PrintArray.java
@@ -1,8 +1,9 @@
 package zadatak_1;
 
 public class PrintArray {
+    private static int [] niz={1,2,3,4,5,6,7,8,9,10};
+
     public static void main(String[] args) {
-        int [] niz={1,2,3,4,5,6,7,8,9,10};
         System.out.println("clanovi niza su");
         for (int i:  niz){
             System.out.println(i);
