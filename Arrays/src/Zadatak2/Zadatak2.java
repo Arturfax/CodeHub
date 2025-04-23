9249a1401847b7d161219296124ec97318e47bd9
Luka <artukovfax@gmail.com>
2025-04-23 08:28:35 +0200

    inicial commit

---
+++ Arrays/src/Zadatak2/Zadatak2.java
@@ -0,0 +1,16 @@
+package Zadatak2;
+
+public class Zadatak2 {
+    private static int[] niz={1,2,3,4,5,6,7,8,9,10};
+
+    public static void ispis(){
+        for(int j=0;j<niz.length;j++){
+            System.out.print(niz[j]+" ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ispis();
+    }
+}
