9f2a79403f019d64cfe1a833a4fc0a2b4cbdb043
Luka <artukovfax@gmail.com>
2025-04-23 09:54:58 +0200

    Inicial commit
    Promena Zadatka3

--- Arrays/src/Zadatak3/Zadatak3.java
+++ Arrays/src/Zadatak3/Zadatak3.java
@@ -3,16 +3,16 @@ package Zadatak3;
 public class Zadatak3 {
     private static int[] niz={4,6,5,7,88,9};
 
-    public static int getSum() {
+    public static int getSum(int[] array) {
         int ukupno = 0;
-        for (int i=0;i<niz.length;i++) {
-            ukupno += niz[i];
+        for (int broj: array) {
+            ukupno += broj;
         }
         return ukupno;
 
     }
     public static void main(String[] args) {
-        System.out.println("Suma brojeva niza je:"+ getSum());
+        System.out.println("Suma brojeva niza je:"+ getSum(niz));
 
     }
 }
